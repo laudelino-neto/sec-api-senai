533ba6d9fe0d43639030b2cab0fa14587c1805a1
Laudelino M. C. Neto <fysout@gmail.com>
2022-03-25 22:21:55 -0300

    Primeiro commit

---
+++ src/main/java/br/com/senai/secapisenai/ValidadorDeAcesso.java
@@ -0,0 +1,14 @@
+package br.com.senai.secapisenai;
+
+public class ValidadorDeAcesso {
+	
+	public void validar(Usuario usuario) {
+		if (!usuario.getLogin().equals("user1") 
+				|| !usuario.getSenha().equals("123")) {
+			throw new IllegalArgumentException(
+					"O usuário " + usuario.getLogin() 
+					+" ou a senha são inválidos");
+		}		
+	}
+	
+}
